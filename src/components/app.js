bb7d92a037d2aba28615394a646ac2748e70e404
GeoHernandez27 <leopardo2@gmail.com>
2019-06-13 16:00:30 -0600

    fixed page scroll refresh bug

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-
+import { BrowserRouter, Route, Switch, withRouter } from "react-router-dom";
 
 import MainPage from "./mainPage";
 import errorPage from "./error";
@@ -14,27 +13,32 @@ import experiencePaddle from "./experiences/paddleExperience";
 import experienceRiver from "./experiences/riverExperience";
 import experienceRoadBike from "./experiences/roadBikeExperience";
 import experienceSki from "./experiences/skiExperience";
+import ScrollToTop from "./scrollToTop";
 
 export default class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>
+          <HamburgerMenu className="hamburger-menu__wrapper" />
+          <ScrollToTop>
+            <Switch>
+              <Route path="/" component={MainPage} exact />
+              <Route path="/rockClimb" component={experienceRockClimb} />
+              <Route path="/hike" component={experienceHike} />
+              <Route path="/mountainBike" component={experienceMountainBike} />
+              <Route
+                path="/mountaineering"
+                component={experienceMountaineering}
+              />
+              <Route path="/paddle" component={experiencePaddle} />
+              <Route path="/river" component={experienceRiver} />
+              <Route path="/roadBike" component={experienceRoadBike} />
+              <Route path="/ski" component={experienceSki} />
 
-        <HamburgerMenu className='hamburger-menu__wrapper'/>
-          <Switch>
-            <Route path="/" component={MainPage} exact />
-            <Route path="/rockClimb" component={experienceRockClimb} />
-            <Route path="/hike" component={experienceHike} />
-            <Route path="/mountainBike" component={experienceMountainBike} />
-            <Route path="/mountaineering" component={experienceMountaineering} />
-            <Route path="/paddle" component={experiencePaddle} />
-            <Route path="/river" component={experienceRiver} />
-            <Route path="/roadBike" component={experienceRoadBike} />
-            <Route path="/ski" component={experienceSki} />
-
-            <Route component={errorPage} exact />
-          </Switch>
+              <Route component={errorPage} exact />
+            </Switch>
+          </ScrollToTop>
         </div>
       </BrowserRouter>
     );
