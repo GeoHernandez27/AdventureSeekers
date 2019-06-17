2d321e843c195ac2d31162bdf258e3d90ad97877
GeoHernandez27 <leopardo2@gmail.com>
2019-06-17 11:19:14 -0600

    updated locals sections

--- src/components/localComponent.js
+++ src/components/localComponent.js
@@ -3,63 +3,82 @@ import React, { Component } from "react";
 export default class LocalComponent extends Component {
   render() {
     return (
-      <div className='local-user-container'>
+      <div className="local-user-container">
         <div className="user-wrapper">
           <div className="user-image">
-          <img
-                src={require("../../static/assets/images/locals/local1.png")}/> 
-          
+            <img
+              src={require("../../static/assets/images/locals/hikeGuide1.png")}
+            />
           </div>
           <div className="user-text__wrapper">
             <div className="user-text__content">
-              <h4>John Smith</h4>
+              <h4>Cora Vasquez</h4>
               <p>
-              "Ssimply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. " 
+                “I love hiking in Salt Lake City because the mountains are
+                really steep and you can access them right from the heart of the
+                city. The sunsets and sunrises are also incredible along the
+                Wasatch Mountains, so try to time your hikes accordingly.”
               </p>
             </div>
           </div>
         </div>
         <div className="user-wrapper">
           <div className="user-image">
-          <img
-                src={require("../../static/assets/images/locals/local1.png")}/> 
-          
+            <img
+              src={require("../../static/assets/images/locals/mountainBGuide2.png")}
+            />
           </div>
           <div className="user-text__wrapper">
-            <div className="user-text__content"><h4>John Smith</h4>
+            <div className="user-text__content">
+              <h4>Dale Sherman</h4>
               <p>
-              "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.  "
-              </p></div>
+                "Utah is not only home to huge networks of mountain bike trails
+                in various areas, it's also an adventurer's playground. Utah has it all. One
+                of our favourite spots to explore is Zion National Park and
+                Arches National Park, where beautiful natural features have been
+                carved by wind and rain over the millenia. "
+              </p>
+            </div>
           </div>
         </div>
         <div className="user-wrapper">
           <div className="user-image">
-          <img
-                src={require("../../static/assets/images/locals/local1.png")}/> 
-          
+            <img
+              src={require("../../static/assets/images/locals/skiGuide1.png")}
+            />
           </div>
           <div className="user-text__wrapper">
-            <div className="user-text__content"><h4>John Smith</h4>
+            <div className="user-text__content">
+              <h4>Kayla Reynolds</h4>
               <p>
-              "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. "
-              </p></div>
+                “I think a great place to stop after a long day of skiing is
+                Porcupine Grill. It’s on the way home from any of the Cottonwood
+                Canyon resorts. It’s definitely some of the best food in Utah
+                and hits the spot after a good day of shredding. Don’t worry
+                about going in your ski clothes, that’s the vibe there.”
+              </p>
+            </div>
           </div>
         </div>
         <div className="user-wrapper">
           <div className="user-image">
-          <img
-                src={require("../../static/assets/images/locals/local1.png")}/> 
-          
+            <img
+              src={require("../../static/assets/images/locals/rockGuide2.png")}
+            />
           </div>
           <div className="user-text__wrapper">
-            <div className="user-text__content"><h4>John Smith</h4>
+            <div className="user-text__content">
+              <h4>Mike Ortega</h4>
               <p>
-              "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, "
-              </p></div>
+                “If you approach climbing with psych and motivation, but also
+                with general self-preservation and caution, there’s nothing you
+                can’t do in Utah. Find the right people and get
+                out there.”
+              </p>
+            </div>
           </div>
         </div>
       </div>
-      
     );
   }
 }
